c5fc11f1d66693e60b94bd7f95fa4c53823606b6
Greycher <akgun.talip.ali@gmail.com>
2022-04-09 22:07:54 +0300

    project context package installed from github

--- Packages/com.greycher.projectcontext/Runtime/GameSetting.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = nameof(GameSetting), menuName = "ScriptableObjects/" + nameof(GameSetting), order = 1)]
-public class GameSetting : ScriptableObject
-{
-    public float PlayerSpeed;
-}
